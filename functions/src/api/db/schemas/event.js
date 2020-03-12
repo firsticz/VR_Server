3e633e8ba0f3cb5bc01a2dc0e7b09de7b40ad49f
First <first@thai.run>
2020-03-12 14:12:58 +0700

    add query activityhasevent

--- functions/src/api/db/schemas/event.js
+++ functions/src/api/db/schemas/event.js
@@ -10,7 +10,71 @@ const eventSchema = new Schema({
   city: String,
   country: String,
   start_date: Date,
-  end_date: Date
+  end_date: Date,
+  member: [Number]
+})
+
+eventSchema.static('activityhasevent', async function activityhasevent(){
+  const recEvent = await this.find({eventId: 100})
+  const records = await this.aggregate([
+    {
+      $match: {
+        eventId: 100
+      }
+    },
+    {
+      $unwind: '$member'
+    },
+    {
+      $group: {
+        _id: '$member',
+        event: {
+          $push: {
+            eventId: '$eventId',
+            nameTH: '$nameTH',
+            start_date : '$start_date',
+            end_date : '$end_date'
+          }
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: 'activitys',
+        localField: '_id',
+        foreignField: 'athlete.id',
+        as: 'activities'
+      }
+    },
+    {
+      $unwind: '$activities'
+    },
+    {
+      $match: {
+        $expr: {
+          $and: [
+            {
+              $gte: [{$toDate:'$activities.start_date'},
+              {$toDate: recEvent[0].start_date}]
+            },{
+              $lte: [{$toDate:'$activities.start_date'},
+              {$toDate: recEvent[0].end_date}]
+            }
+          ]
+        }
+      }
+    },{
+      $group: {
+        _id: '$_id',
+        event: {$first: '$event'},
+        activities: {$push: '$activities'}
+      }
+    }
+  ])
+  // console.log(recEvent)
+  // console.log('start')
+  // console.log(records)
+  return records
 })
 
 
--- functions/src/api/graphql/queries/event.js
+++ functions/src/api/graphql/queries/event.js
@@ -1,11 +1,34 @@
 const eventsTC = require('../type-composers/event')
 const events = require('../../db/models/event')
+const activityTC = require('../type-composers/activity')
 
 const {
   schemaComposer, Resolver, graphql: {
-    GraphQLString, GraphQLObjectType, GraphQLList, GraphQLFloat, GraphQLInt,
+    GraphQLString, GraphQLObjectType, GraphQLList, GraphQLFloat, GraphQLInt, Gr
   },
 } = require('graphql-compose')
+
+const activityhasevent = new Resolver({
+  name: 'activityhasevent',
+  type: new GraphQLList(new GraphQLObjectType({
+    name: 'activityhaseventType',
+    fields: {
+      _id: {
+        type: GraphQLString,
+      },
+      event: {
+        type: new GraphQLList(eventsTC.getType())
+      },
+      activities: {
+        type: new GraphQLList(activityTC.getType())
+      }
+    }
+  })),
+  resolve: async () => {
+    const data = await events.activityhasevent()
+    return data
+  },
+},schemaComposer)
 
 
 const event = {
@@ -16,5 +39,6 @@ const event = {
   eventCount: eventsTC.getResolver('count'),
   eventConnection: eventsTC.getResolver('connection'),
   eventPagination: eventsTC.getResolver('pagination'),
+  activityhasevent,
 }
 module.exports = event
