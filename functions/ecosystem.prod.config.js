3c358c968fe02f06e2bddb8d1db36649669e9952
First <first@thai.run>
2020-04-14 16:32:42 +0700

    refactor code

--- functions/ecosystem.prod.config.js
+++ functions/ecosystem.prod.config.js
@@ -3,24 +3,5 @@ module.exports = {
     name: 'strava',
     script: 'dist/index.js',
   }],
-
-  // deploy: {
-  //   production: {
-  //     user: 'app',
-  //     host: 'hooks.chat.thai.run',
-  //     ref: 'origin/master',
-  //     repo: 'git@bitbucket.org:Thairun/photo-messenger-bot.git',
-  //     path: '/home/app/production',
-  //     'post-deploy': 'npm install && npm run build && npm run start',
-  //   },
-  //   development: {
-  //     user: 'dev',
-  //     host: '',
-  //     ref: 'origin/develop',
-  //     repo: 'git@bitbucket.org:Thairun/photo-messenger-bot.git',
-  //     path: '/home/dev/development',
-  //     'post-deploy': 'npm install && npm run build && npm run start',
-  //   },
-  // },
 }
 
