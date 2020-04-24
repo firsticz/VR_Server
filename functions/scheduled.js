cc6f68a25b5c45449005b4c15995c89047b22b94
First <first@thai.run>
2020-04-24 13:18:36 +0700

    edit function scheduled

--- functions/scheduled.js
+++ functions/scheduled.js
@@ -9,15 +9,19 @@ exports.scheduled = functions
   .region('asia-east2')
   .pubsub
   // .schedule('*/5 * * * *') // every 5 minutes
-  .schedule('every 1 minutes') // every 5 minutes
+  .schedule('every 5 minutes') // every 5 minutes
   .timeZone('Asia/Bangkok') // Users can choose timezone - default is America/Los_Angeles
   .onRun((context) => {
     // console.log('RUNNGING schedulted', new Date())
-    return Promise.each([
-      'updateactivity',
-    ], (resource) => {
-      return Activitys[resource].cache(900, null, true).exec()
-    })
+    // return Promise.each([
+    //   'updateAllActivity',
+    // ], (resource) => {
+    //   return Activitys[resource].cache(900, null, true).exec()
+    // })
     // console.log('running', context)
     // return null
+    const status = Activitys.updateAllActivity()
+    if(status){
+      console.log("success")
+    }
   })
